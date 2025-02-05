85edcc1def98a051c693756a6d3710a18d94971d
Joe Reeve <joe.reeve@gmail.com>
2025-02-05 16:14:35 +0900

    chore: remove unused imports

--- client/src/components/LogInPage.tsx
+++ client/src/components/LogInPage.tsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react';
-
 const LogInPage = () => {
   return (
     <div className="log-in-page">
--- client/src/components/ProfilePage.tsx
+++ client/src/components/ProfilePage.tsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react';
-
 const ProfilePage = () => {
   return (
     <div className="profile-page">
