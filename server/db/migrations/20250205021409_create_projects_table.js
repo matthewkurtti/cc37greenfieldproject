19882d299a0e200b0e3105feb4a02ff02f5ccf48
sylentwulf81 <dlavizzo@gmail.com>
2025-02-05 15:48:31 +0900

    chore: deletes superflous columns

--- server/db/migrations/20250205021409_create_projects_table.js
+++ server/db/migrations/20250205021409_create_projects_table.js
@@ -6,7 +6,6 @@ exports.up = function (knex) {
     return knex.schema.createTable("projects", function (table) {
       table.increments("id").primary();
       table.string("project_name", 30);
-      table.string("stem_id"); // adds stem_id for foreign key relation
       table.text("description", 100).comment("brief project description");
       table.timestamps(true, true);
     });
--- server/db/seeds/03_projects.js
+++ server/db/seeds/03_projects.js
@@ -9,25 +9,21 @@ exports.seed = async function (knex) {
       id: 1,
       project_name: "Summer Collab",
       description: "Upbeat summer track collaboration",
-      stem_id: stems[0].id,
     },
     {
       id: 2,
       project_name: "Love Fest",
       description: "Slow jam love song, needs vocals",
-      stem_id: stems[1].id,
     },
     {
       id: 3,
       project_name: "Outrun",
       description: "smooth vaporwave classic, needs mastering",
-      stem_id: stems[2].id,
     },
     {
       id: 4,
       project_name: "Alone...",
       description: "emo indie shoegaze",
-      stem_id: stems[3].id,
     },
   ]);
 };
