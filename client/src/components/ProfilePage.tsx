41a6174ba4a147e666eff8533eff61d69636bafd
Matt K <matthew.b.kurtti@gmail.com>
2025-02-12 14:34:08 +0900

    feat: add upload profile pic button element

--- client/src/components/ProfilePage.tsx
+++ client/src/components/ProfilePage.tsx
@@ -1,9 +1,9 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
-import { getData } from '../helpers/fetchHelpers';
-import { User } from '../globalTypes';
-import './ProfilePage.css';
-import avatar from '../assets/avatar.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { getData } from "../helpers/fetchHelpers";
+import { User } from "../globalTypes";
+import "./ProfilePage.css";
+import avatar from "../assets/avatar.png";
 
 type ProfilePageProps = {
   loggedInUser: User | null;
@@ -13,7 +13,7 @@ type ProfilePageProps = {
 const ProfilePage = ({ loggedInUser, setCurrentModal }: ProfilePageProps) => {
   // changes database target URL depending on current environment
   const url: string =
-    import.meta.env.MODE === 'development' ? 'http://localhost:8080/' : '/';
+    import.meta.env.MODE === "development" ? "http://localhost:8080/" : "/";
 
   return (
     <div className="profile-page">
@@ -21,6 +21,7 @@ const ProfilePage = ({ loggedInUser, setCurrentModal }: ProfilePageProps) => {
       {loggedInUser && (
         <>
           <img className="avatar" src={avatar} alt="Your profile picture" />
+          <button>Upload Profile Pic</button>
           <label>USERNAME</label>
           <p>{loggedInUser.username}</p>
           {loggedInUser.city && (
@@ -37,11 +38,11 @@ const ProfilePage = ({ loggedInUser, setCurrentModal }: ProfilePageProps) => {
       <button
         type="button"
         onClick={async () => {
-          await getData(url, 'api/auth/logout');
+          await getData(url, "api/auth/logout");
           setCurrentModal(null);
         }}
       >
-        Log Out{' '}
+        Log Out{" "}
         <span className="nav-icon">
           <FontAwesomeIcon
             icon={faRightToBracket}
